1e4921635ff53ea9e198c06a8e666a8d541cecdb
vanikrishna-dev <vanikrishna02@gmail.com>
2026-04-03 22:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+(function () {
+  'use strict';
+
+  window.addEventListener('load', function () {
+    const forms = document.getElementsByClassName('needs-validation');
+
+    Array.prototype.forEach.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
